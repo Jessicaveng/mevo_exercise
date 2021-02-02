e27fba4287554f228fae5f24c9d62bc5565e0e3c
Jessicaveng <jessicaveng@outlook.com>
2021-02-03 02:10:15 +1300

    Edited components + scss files

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,16 @@
-import './Scss/index.scss'
+import React from 'react'
+
+import '../Scss/index.scss'
+import Nav from './Nav.jsx'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   
       </header>
-
+      <Nav/>
+      <hr/>
     </div>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,24 @@
-import './Scss/index.scss'
-import logodark  from './logodark.svg'
+import '../Scss/index.scss'
+import logodark  from '../logodark.svg'
 
 function Nav() {
   return (
     <div className="Nav">
         <img src={logodark} className="logodark" alt="logo" />
-      
+        <div className="nav-link">  
+          <a>How It Works </a>
+          <a>Pricing </a>
+          <a>Home Zone</a>
+          <a>Business</a>
+          <a>FAQS</a>
+          <button type="button"> Sign In </button>
+          <button type="button"> Join For Free</button>
+          </div>
+       
+
+
     </div>
   )
 }
+
+export default Nav
