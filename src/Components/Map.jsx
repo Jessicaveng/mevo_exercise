c3dc2f44479ee0aea41f2769da674c89fd1bd7dd
Jessicaveng <jessicaveng@outlook.com>
2021-02-05 12:01:58 +1300

    Displayed cluster

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -3,13 +3,15 @@ import React, { useState, useRef } from "react";
 //import mapboxgl from "mapbox-gl";
 import useSwr from "swr";
 import ReactMapGL, { Marker } from "react-map-gl";
+import useSupercluster from "use-supercluster";
+
 import "../Scss/index.scss";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const MapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-const fetcher = (...args) => fetch(...args).then(response => response.json()); //FETCH ARGS  THEN CONVERT RESPONSE TO JSON
+const fetcher = (...args) => fetch(...args).then((response) => response.json()); //FETCH ARGS  THEN CONVERT RESPONSE TO JSON
 
 //SET UP MAP
 export default function Map() {
@@ -25,33 +27,82 @@ export default function Map() {
   const mevoVehiclesApi = "https://api.mevo.co.nz/public/vehicles/all";
   const { data, error } = useSwr(mevoVehiclesApi, fetcher);
   const vehicles = data && !error ? data.slice(0, 70) : [];
+  const points = vehicles.map((vehicle) => ({
+    properties: {
+      cluster: false,
+      icon: "../logo.svg",
+    },
+    geometry: {
+      coordinates: [vehicle.position.longitude, vehicle.position.latitude],
+    },
+  }));
+
+  //GET MAP BOUNDS
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  //GET CLUSTERS
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds, //4 cornes of lng and Lat
+    zoom: viewport.zoom,
+    options: { radius: 15, maxZoom: 20 },
+  });
 
   return (
-    <ReactMapGL
-      {...viewport}
-      width="100vw"
-      height="50vh"
-      maxZoom={20}
-      mapstyle="mapbox://styles/mapbox/streets-v11"
-      mapboxApiAccessToken={MapboxAccessToken}
-      onViewportChange={(newViewport) => {
-        //UPDATE VIEWPORT WHEN USER DRAGS/ZOOMS IN/OUT
-        setViewport({ ...newViewport });
-      }}
-      ref={mapRef}
-    >
-      {/* {marker} */}
-      {vehicles.map((vehcile) => (
-        <Marker
-          key={vehcile}
-          latitude={parseFloat(vehcile.position.latitude)}
-          longitude={parseFloat(vehcile.position.longitude)}
-        >
-          <button>
-            <img src={vehcile.iconUrl} alt="vehicle coordinates" />
-          </button>
-        </Marker>
-      ))}
-    </ReactMapGL>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        width="100vw"
+        height="80vh"
+        maxZoom={20}
+        mapstyle="mapbox://styles/mapbox/streets-v11"
+        mapboxApiAccessToken={MapboxAccessToken}
+        onViewportChange={(newViewport) => {
+          //UPDATE VIEWPORT WHEN USER DRAGS/ZOOMS IN/OUT
+          setViewport({ ...newViewport });
+        }}
+        ref={mapRef} // GIVE ACCESS TO THE MAP ITSELF
+      >
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount,
+          } = cluster.properties;
+          if (isCluster) {
+            return (
+              <Marker
+                key={cluster.id}
+                longitude={parseFloat(longitude)}
+                latitude={parseFloat(latitude)}
+              >
+                <div
+                  className="cluster-marker"
+                  style={
+                    ({ width: `${10 + (pointCount / points.length) *20}px` },
+                    { height: `${10 + (pointCount / points.length) *20}px`})
+                  }
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+          return (
+            <Marker
+              key={cluster.properties.id}
+              latitude={parseFloat(latitude)}
+              longitude={parseFloat(longitude)}
+            >
+              <button className="vehicle-marker">
+                <img src="vehiclemarker.png" alt="vehicle coordinates" />
+              </button>
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
+    </div>
   );
 }
