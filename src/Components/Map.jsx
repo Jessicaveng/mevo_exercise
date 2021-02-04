7e9ec19a2e75fdc79d88586720c229fbf8eab7fa
Jessicaveng <jessicaveng@outlook.com>
2021-02-05 00:07:15 +1300

    displayed markers

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,48 +1,57 @@
-
 import React, { useState, useRef } from "react";
 //import ReactDOM from 'react-dom';
 //import mapboxgl from "mapbox-gl";
-import ReactMapGL, {Marker}from 'react-map-gl'
-//import Vehicles from "./Vehicles.jsx";
-//import {mevoVehicles} from '../Api/api'
-import '../Scss/index.scss'
-import dotenv from 'dotenv'
-dotenv.config()
+import useSwr from "swr";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "../Scss/index.scss";
 
-const MapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+import dotenv from "dotenv";
+dotenv.config();
 
-  //SET UP MAP  
-  export default function Map () {
-    const [viewport, setViewport]=useState({
-      longitude: 174.777969,
-      latitude: -41.276825,
-      zoom: 13
-    })
+const MapboxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+const fetcher = (...args) => fetch(...args).then(response => response.json()); //FETCH ARGS  THEN CONVERT RESPONSE TO JSON
 
-    const mapRef = useRef();
+//SET UP MAP
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    longitude: 174.777969,
+    latitude: -41.276825,
+    zoom: 13,
+  });
 
+  const mapRef = useRef();
 
-    //FETCH MEVO VEHICLES API DATA
-    const mevoVehiclesApi = "https://api.mevo.co.nz/public/vehicles/all";
-    
-    return (
-      <ReactMapGL
-        {...viewport} 
-        width= '100vw'
-        height='50vh'
-        maxZoom={20}         
-        mapstyle= "mapbox://styles/mapbox/streets-v11"
-        mapboxApiAccessToken={MapboxAccessToken}
-        
-        onViewportChange={newViewport =>{ //UPDATE VIEWPORT WHEN USER DRAGS/ZOOMS IN/OUT
-          setViewport({...newViewport})
-        }}
-        ref={mapRef}
-      > 
-        {/* {marker} */}
-      </ReactMapGL>
-    )
-  }
+  //FETCH MEVO VEHICLES API DATA
+  const mevoVehiclesApi = "https://api.mevo.co.nz/public/vehicles/all";
+  const { data, error } = useSwr(mevoVehiclesApi, fetcher);
+  const vehicles = data && !error ? data.slice(0, 70) : [];
 
- 
-
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="100vw"
+      height="50vh"
+      maxZoom={20}
+      mapstyle="mapbox://styles/mapbox/streets-v11"
+      mapboxApiAccessToken={MapboxAccessToken}
+      onViewportChange={(newViewport) => {
+        //UPDATE VIEWPORT WHEN USER DRAGS/ZOOMS IN/OUT
+        setViewport({ ...newViewport });
+      }}
+      ref={mapRef}
+    >
+      {/* {marker} */}
+      {vehicles.map((vehcile) => (
+        <Marker
+          key={vehcile}
+          latitude={parseFloat(vehcile.position.latitude)}
+          longitude={parseFloat(vehcile.position.longitude)}
+        >
+          <button>
+            <img src={vehcile.iconUrl} alt="vehicle coordinates" />
+          </button>
+        </Marker>
+      ))}
+    </ReactMapGL>
+  );
+}
